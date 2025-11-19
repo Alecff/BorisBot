4ed6655cba2156a8ef34506a143f3071d09852f0
Alec Foster <alecfoster412@gmail.com>
2025-11-19 13:47:42 +0000

    Add stuff with commands

---
+++ Bot/testRequest.py
@@ -0,0 +1,6 @@
+import requests
+
+requests.post(
+    "https://hub-opia-cloud.dev.opiahost.co.uk/api/v1/claim",
+    data={"key": "value"}
+)
--- run-env.sh
+++ run-env.sh
@@ -1,2 +1,2 @@
+#!/bin/bash
 python3 -m venv bot-env
-&& source bot-env/bin/activate
